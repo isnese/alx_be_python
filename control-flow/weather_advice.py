29f12818014373282014863f381951625a18b956
🌘AhmedABD <109002253+isnese@users.noreply.github.com>
2024-06-30 17:11:08 +0300

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-user_input = input("What's the weather like today? (sunny/rainy/cold").strip().lower()
+input = input("What's the weather like today? (sunny/rainy/cold").strip().lower()
 sunny = 'Wear a t-shirt and sunglasses'
 rainy = "Don't forget your umbrella and a raincoat"
 cold  = 'Make sure to wear a warm coat and a scarf'
