d038c2727a71a6cb8daab84d55a77a668f7f7fd1
🌘AhmedABD <109002253+isnese@users.noreply.github.com>
2024-07-18 13:21:31 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 def perform_operation(num1, num2, operation) :
     if operation == 'add' :
-         return num1+num2
+         return num1 + num2
     elif operation == 'subtract' :
-         return num1-num2
+         return num1 - num2
     elif operation == 'multiply' :
-         return num1*num2
+         return num1 * num2
     elif operation == 'divide' and num1 == 0 or num2 == 0:
          return 'Error: Division by zero is not allowed'
     elif operation == 'divide' and num1 != 0 and num2 != 0:
-         return num1/num2
+         return num1 / num2
