d5e04ce6f94cecf2387a758e3774fca5e5556756
🌘AhmedABD <109002253+isnese@users.noreply.github.com>
2024-07-18 12:08:28 +0300

    Add files via upload

---
+++ fns_and-dsa/arithmetic_operations.py
@@ -0,0 +1,12 @@
+def perform_operation(num1, num2, operation) :
+    if operation == 'add' :
+         return num1 + num2
+    elif operation == 'subtract' :
+         return num1 - num2
+    elif operation == 'multiply' :
+         return num1 * num2
+    elif operation == 'divide' and num1 == 0 or num2 == 0:
+         return 'Error: Division by zero is not allowed'
+    elif operation == 'divide' and num1 != 0 and num2 != 0:
+         return num1 / num2
+    
