2165eaaa3c6eb7acd81109f8c3725be697e5868a
🌘AhmedABD <109002253+isnese@users.noreply.github.com>
2024-07-18 17:02:17 +0300

    Add files via upload

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,71 @@
+import datetime
+
+def display_current_datetime() :
+    # Get the current date and time
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Print the current date and time
+    print("Current date and time:", current_date)
+
+display_current_datetime()
+
+def calculate_future_date():
+    number_of_days = int(input('Enter the number of days to add to the current date: '))
+     # Get the current date
+    current_date = datetime.datetime.now().date()
+     # Calculate the future date
+    future_date = current_date + datetime.timedelta(days=number_of_days)
+    # Format the future date as "YYYY-MM-DD"
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print(f'Future date: {formatted_future_date}')
+calculate_future_date()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Get the current date and time
+##############################
+# def display_current_datetime() :
+##############################
+#     current_date = datetime.datetime.now()
+##############################
+#     # formatted_current_date
+
+#     # Format the current date and time
+#     formatted_current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+#     # print(f'Current date and time: {formatted_current_date}')
+#     return formatted_current_date
+
+#######################
+# display_current_datetime() #done
+#######################
+
+
+
+
+
+
+
+
+
+
