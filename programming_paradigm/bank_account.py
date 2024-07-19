2cd03ddfb226bb4dfa8dbcc8a05285c57a71b573
🌘AhmedABD <109002253+isnese@users.noreply.github.com>
2024-07-19 14:56:14 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        # Ensure the balance is printed with two decimal places
+        print(f"Current Balance: ${self.account_balance:.2f}")
